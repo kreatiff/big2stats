70b3e14e14a1b4306a13f187f9f21ca67d6f3920
Oleg Estrin <oleg.estrin@gmail.com>
2011-05-17 17:13:03 +1000

    beta

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,5 +46,5 @@ Big2stats::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => 'http://big2stats.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'big2stats.heroku.com' }
 end
